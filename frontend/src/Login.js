8be33e94fee943bbdff27ddb9e8913cfee8016e8
cosminlup16 <cosmin.lup@8x8.com>
2023-12-12 22:45:18 +0200

    setup login page and connect backend to frontend

---
+++ frontend/src/Login.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const Login = (props) => (
+<div className="Login-form-container">
+      <form className="Login-form">
+        <div className="Login-form-content">
+          <h3 className="Login-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+)
+
+export default Login
